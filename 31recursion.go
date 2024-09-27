1db9584292b111649873590fc53d404a4869c7eb
rubayet170746 <rafirubayet75@gmail.com>
2024-09-27 21:20:22 +0600

    added recursion

---
+++ 31recursion.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+var pl = fmt.Println
+
+func factorial(num int) int {
+	// This condition ends calling functions
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
+
+func main(){
+
+	// ----- RECURSION -----
+	// Recursion occurs when a function calls itself
+	// There must be a condition that ends this
+	// Finding a factorial is commonly used
+	pl("Factorial 4 =", factorial(4))
+	// 1st : result = 4 * factorial(3) = 4 * 6 = 24
+	// 2nd : result = 3 * factorial(2) = 3 * 2 = 6
+	// 3rd : result = 2 * factorial(1) = 2 * 1 = 2
+	
+} 

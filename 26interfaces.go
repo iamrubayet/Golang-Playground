2e671d0fc481475d2a9dd3d9afc531d3f731aafc
rubayet170746 <rafirubayet75@gmail.com>
2024-09-27 12:34:35 +0600

    interface added

--- 26interfaces.go
+++ 26interfaces.go
@@ -9,5 +9,33 @@ import (
 var pl = fmt.Println
 
 func main(){
+	type Animal interface {
+	AngrySound()
+	HappySound()
+}
+
+// Define type with interface methods and its
+// own method
+type Cat string
+
+func (c Cat) Attack() {
+	pl("Cat Attacks its Prey")
+}
+
+// Return the cats name with a type conversion
+func (c Cat) Name() string {
+	return string(c)
+}
+
+//automatically knows not like java or python. automatically knows it want to part of this interface
+
+
+
+func (c Cat) AngrySound() {
+	pl("Cat says Hissssss")
+}
+func (c Cat) HappySound() {
+	pl("Cat says Purrr")
+}
 	
 } 
